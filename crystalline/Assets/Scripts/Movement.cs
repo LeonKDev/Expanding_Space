a174a8ce2c0feef6808c8de4b69a3b77612dce77
leon kruijer <32770@ma-web.nl>
2022-05-12 11:19:34 +0200

    test worldV1

--- crystalline/Assets/Scripts/Movement.cs
+++ crystalline/Assets/Scripts/Movement.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    private Animator animator;
+    
     private Rigidbody rb;
     public float jumpPower = 8;
     public bool canJump = true;
@@ -12,25 +12,25 @@ public class Movement : MonoBehaviour
     void Start()
     {
         rb = gameObject.GetComponent<Rigidbody>();
-        animator = GetComponent<Animator>();
+        
     }
 
     void Update()
     {
-        animator.SetFloat("Speed", 0);
+      
         if (Input.GetKey(KeyCode.A))
         {
-            animator.SetFloat("Speed", 1);
+            
             transform.position += new Vector3(-movementSpeed, 0, 0) * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.D))
         {
-            animator.SetFloat("Speed", -1);
+            
             transform.position += new Vector3(movementSpeed, 0, 0) * Time.deltaTime;
         }
         if (Input.GetKeyDown(KeyCode.W) && canJump == true)
         {
-            animator.SetTrigger("jump");
+            
             rb.velocity = new Vector3(0, jumpPower, 0);
         }
         if (Input.GetKeyDown(KeyCode.S) && canJump == true)
