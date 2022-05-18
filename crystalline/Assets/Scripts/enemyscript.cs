528ec702d3e2875b854124f05bbcec4d6cb1723f
EdRma <34298@ma-web.nl>
2022-05-18 11:43:32 +0200

    enemy

--- crystalline/Assets/Scripts/enemyscript.cs
+++ crystalline/Assets/Scripts/enemyscript.cs
@@ -5,14 +5,18 @@ using UnityEngine.AI;
 
 public class enemyscript : MonoBehaviour
 {
-    public NavMeshAgent enemy;
-    public Transform player;
+    public float speed;
+    private Transform target;
     void Start()
     {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
     }
-
-    void fixedUpdate()
+    void Update()
     {
-        enemy.SetDestination(player.position);
+        if(Vector3.Distance(transform.position, target.position) > 3 )
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }
+        
     }
 }
