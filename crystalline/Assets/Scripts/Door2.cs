41f7c8c4efa0583db5ae4a235ac18ef6308efd36
Bas Klaichid <32817@ma-web.nl>
2022-06-29 15:02:00 +0200

    deur

---
+++ crystalline/Assets/Scripts/Door2.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+public class Door2 : MonoBehaviour
+{
+    public bool IsInRange;
+
+
+    void Start()
+    {
+
+    }
+
+
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+
+
+
+    void Update()
+    {
+        if (IsInRange)
+
+
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                SceneManager.LoadScene(3);
+            }
+
+
+
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                SceneManager.LoadScene(2);
+            }
+        }
+
+
+
+
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            IsInRange = true;
+            Debug.Log("Player is now in Range");
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        IsInRange = false;
+        Debug.Log("Player is now in Range");
+    }
+}
