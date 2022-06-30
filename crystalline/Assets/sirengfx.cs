f16e015638e22157ce99571085230e89e01f1731
EdRma <34298@ma-web.nl>
2022-06-30 12:15:41 +0200

    animation siren

--- crystalline/Assets/sirengfx.cs
+++ crystalline/Assets/sirengfx.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pathfinding;
 
 public class sirengfx : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public AIPath aipath;
 
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        if(aipath.desiredVelocity.x >= 0.01f)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+
+        }else if(aipath.desiredVelocity.x <= -0.01f)
+        {
+            transform.localScale = new Vector3(1f, 1f, 1f);
+        }
     }
 }
